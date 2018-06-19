a54d9e23b8cee185d9f2359870424a2c2a088319
opticfluorine <opticfluorine@gmail.com>
2018-06-18 23:12:34 -0400

    Add components for position and velocity

---
+++ EngineCore/Components/ComponentOperators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine8.EngineCore.Components
+{
+
+    /// <summary>
+    /// Standard operators for common component types.
+    /// </summary>
+    public static class ComponentOperators
+    {
+
+        /// <summary>
+        /// Standard operators for float-valued components.
+        /// </summary>
+        public static readonly IDictionary<ComponentOperation, Func<float, float, float>>
+            FloatOperators = new Dictionary<ComponentOperation, Func<float, float, float>>()
+            {
+                {ComponentOperation.Set, (a, b) => a },
+                {ComponentOperation.Add, (a, b) => a + b},
+                {ComponentOperation.Multiply, (a, b) => a * b},
+                {ComponentOperation.Divide, (a, b) => a / b},
+            };
+
+    }
+
+}
---
+++ EngineCore/Systems/Movement/Components/VelocityYComponentCollection.cs
@@ -0,0 +1,21 @@
+﻿using Engine8.EngineCore.Components;
+
+namespace Engine8.EngineCore.Systems.Movement.Components
+{
+
+    /// <summary>
+    /// Component collection for entity velocities along the y axis.
+    /// </summary>
+    public class VelocityYComponentCollection : BaseComponentCollection<float>
+    {
+
+        /// <summary>
+        /// Initial number of components to allocate.
+        /// </summary>
+        private const int BASE_SIZE = 65536;
+
+        public VelocityYComponentCollection() : base(BASE_SIZE, ComponentOperators.FloatOperators) { }
+
+    }
+
+}
