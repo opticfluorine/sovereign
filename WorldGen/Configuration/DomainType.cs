e3d300359dae4679ac2b0f04be15a59b65fb59ec
opticfluorine <opticfluorine@gmail.com>
2018-08-30 18:59:45 -0400

    Continue work on world generation

---
+++ WorldGen/Configuration/DomainType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine8.WorldGen.Configuration
+{
+
+    /// <summary>
+    /// Allowed domain types.
+    /// </summary>
+    public enum DomainType
+    {
+        /// <summary>
+        /// Generates a domain with multiple continents.
+        /// </summary>
+        Continents,
+    }
+
+}
--- WorldGen/Configuration/WorldGenConfiguration.cs
+++ WorldGen/Configuration/WorldGenConfiguration.cs
@@ -11,6 +11,63 @@ namespace Engine8.WorldGen.Configuration
     public class WorldGenConfiguration
     {
 
+        #region Common
+
+        /// <summary>
+        /// Random number generator seed.
+        /// </summary>
+        public int Seed { get; set; }
+
+        /// <summary>
+        /// Width (along x) in blocks.
+        /// </summary>
+        public uint Width { get; set; }
+
+        /// <summary>
+        /// Height (along y) in blocks.
+        /// </summary>
+        public uint Height { get; set; }
+
+        /// <summary>
+        /// Type of domain to generate.
+        /// </summary>
+        public DomainType DomainType { get; set; }
+
+        #endregion Common
+
+        #region Terrain
+
+        /// <summary>
+        /// Material ID used for water.
+        /// </summary>
+        public uint WaterMaterialId { get; set; }
+
+        #endregion Terrain
+
+        #region Continents
+
+        /// <summary>
+        /// Configuration for continent generation.
+        /// </summary>
+        ///
+        /// Only valid if DomainType is set to Continents.
+        public ContinentsConfiguration Continents { get; set; }
+
+        /// <summary>
+        /// Inner configuration class used for continent generation.
+        /// </summary>
+        public class ContinentsConfiguration
+        {
+
+            /// <summary>
+            /// Number of continents to generate.
+            /// </summary>
+            public uint Count { get; set; }
+
+        }
+
+        #endregion Continents
+
     }
 
 }
