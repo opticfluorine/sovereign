b8b21e42628bccbad2c99108b45f23ba36f74102
opticfluorine <opticfluorine@gmail.com>
2024-03-04 23:24:16 -0500

    Start fixing two-way entity sync, but introduced other bugs

--- src/Server/ServerCore/Systems/WorldManagement/WorldManagementSystem.cs
+++ src/Server/ServerCore/Systems/WorldManagement/WorldManagementSystem.cs
@@ -52,7 +52,8 @@ public sealed class WorldManagementSystem : ISystem
 
     public ISet<EventId> EventIdsOfInterest { get; } = new HashSet<EventId>
     {
-        EventId.Server_WorldManagement_WorldSegmentLoaded
+        EventId.Server_WorldManagement_WorldSegmentLoaded,
+        EventId.Core_WorldManagement_EntityLeaveWorldSegment
     };
 
     public int WorkloadEstimate => 80;
